58feca1603e9a5212f28b8ae1b2194e35f91485f
Ekaterina Starunskaya <starunskaya.katya@yandex.ru>
2025-11-21 15:14:15 +0300

    Написала db_manager для подсчета количества репозиториев

--- src/db_manager.py
+++ src/db_manager.py
@@ -3,6 +3,7 @@ import uuid
 from datetime import datetime
 from typing import Optional
 
+from sqlalchemy import Integer
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlmodel import Field, SQLModel, select, func
 
@@ -23,7 +24,6 @@ class DateBase:
     def __init__(self, db_url: str):
         self.engine = create_async_engine(db_url, echo=False)
         self.session = async_sessionmaker(self.engine, expire_on_commit=False)
-
 
 
     async def init(self) -> None:
@@ -67,15 +67,33 @@ class DateBase:
             date_from: Optional[datetime] = None,
             date_to: Optional[datetime] = None,
     ) -> pd.DataFrame:
+        """Функция считает, сколько репозиториев живет 1, 2, 3... лет"""
         if date_from is None:
             date_from = await self.min_date()
 
         if date_to is None:
             date_to = await self.max_date()
 
-        async with (self.session() as session):
+        async with self.session() as session:
             async with session.begin():
-                lifespan_days_expr = func.julianday(RepoInfo.pushed_at) - func.julianday(RepoInfo.created_at)
+                age_years = func.cast(
+                    (func.julianday("now") - func.julianday(RepoInfo.created_at)) / 365, Integer
+                ).label("age_years")
+                #lifespan_days = func.julianday(RepoInfo.pushed_at) - func.julianday(RepoInfo.created_at)
+                query = (
+                    select(age_years, func.count())
+                    .where(RepoInfo.created_at.between(date_from, date_to))
+                    .group_by(age_years)
+                    )
+                result = await session.execute(query)
+                dataframe = pd.DataFrame(
+                    result.all(),
+                    columns=["age_years", "count"]
+                )
+                dataframe.set_index("age_years", inplace=True)
+                return dataframe
+
+
 
 
 
